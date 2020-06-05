ec801aae07becdf0b11d098bed6277240f869ca9
Verity <58147038+verityyt@users.noreply.github.com>
2020-06-05 21:04:26 +0200

    EarlyAccess screen design upgraded
    Text added, changed the text of the buttons from
       "Stable version","EAP version" to "Stable","Early Access"
       and moved the buttons down

--- src/main/java/net/inceptioncloud/installer/frontend/screens/EarlyAccessScreen.kt
+++ src/main/java/net/inceptioncloud/installer/frontend/screens/EarlyAccessScreen.kt
@@ -1,5 +1,6 @@
 package net.inceptioncloud.installer.frontend.screens
 
+import net.inceptioncloud.installer.Logger
 import net.inceptioncloud.installer.MinecraftModInstaller
 import net.inceptioncloud.installer.frontend.FontManager
 import net.inceptioncloud.installer.frontend.Screen
@@ -20,10 +21,11 @@ class EarlyAccessScreen : Screen(1) {
     /**
      * Button that is clicked to start the setup for the stable version.
      */
-    private val stable: UIButton = object : UIButton("Stable version") {
+    private val stable: UIButton = object : UIButton("Stable") {
 
         override fun buttonClicked() {
             MinecraftModInstaller.screen = PreparingSetupScreen()
+            Logger.log("User selected \"Stable\" for download")
         }
 
     }
@@ -31,11 +33,12 @@ class EarlyAccessScreen : Screen(1) {
     /**
      * Button that is clicked to start the setup for the eap version.
      */
-    private val earlyAccess: UIButton = object : UIButton("EAP version") {
+    private val earlyAccess: UIButton = object : UIButton("Early Access") {
 
         override fun buttonClicked() {
             downloadEAP = true
             MinecraftModInstaller.screen = PreparingSetupScreen()
+            Logger.log("User selected \"Early Access\" for download")
         }
 
     }
@@ -46,16 +49,49 @@ class EarlyAccessScreen : Screen(1) {
     }
 
     override fun paint(graphics2D: Graphics2D, x: Int, y: Int, width: Int, height: Int) {
+        val offset = 4
 
         graphics2D.color = Color(50, 50, 50)
-        FontManager.drawCenteredString("Select Client Version", x + width / 2, y + 160, 0, 30, graphics2D)
+        FontManager.drawCenteredString("Downlaod Channel", x + width / 2, y + 160, 0, 30, graphics2D)
 
-        // Stable button
+        FontManager.drawCenteredString(
+            "Lorem ipsum dolor sit amet, consetetur",
+            x + width / 2 - offset,
+            y + 245,
+            2,
+            22,
+            graphics2D
+        )
+        FontManager.drawCenteredString(
+            "tempor invidunt ut labore et dolore",
+            x + width / 2 - offset,
+            y + 273,
+            2,
+            22,
+            graphics2D
+        )
+        FontManager.drawCenteredString(
+            "sadipscing elitr, sed diam nonumy",
+            x + width / 2 - offset,
+            y + 303,
+            2,
+            22,
+            graphics2D
+        )
+        FontManager.drawCenteredString(
+            "magna aliquyam erat, sed diam.",
+            x + width / 2 - offset,
+            y + 333,
+            2,
+            22,
+            graphics2D
+        )
+        FontManager.drawCenteredString("At vero eos et accusam et", x + width / 2 - offset, y + 363, 2, 22, graphics2D)
+
         val buttonWidth = (width / 2)
         val buttonHeight = buttonWidth / 5.1
-        stable.paint(graphics2D, x + width / 2 - buttonWidth / 2, y + 350, buttonWidth, buttonHeight.toInt())
-        earlyAccess.paint(graphics2D, x + width / 2 - buttonWidth / 2, y + 425, buttonWidth, buttonHeight.toInt())
-
+        stable.paint(graphics2D, x + width / 2 - buttonWidth / 2, y + 435, buttonWidth, buttonHeight.toInt())
+        earlyAccess.paint(graphics2D, x + width / 2 - buttonWidth / 2, y + 485, buttonWidth, buttonHeight.toInt())
     }
 
     override fun mouseClicked(event: MouseEvent?) {
