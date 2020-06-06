724ba3b1a54aa2147be4c74f471d47e1ce46199a
verity <joshua-schmidt@online.de>
2020-06-06 14:19:37 +0200

    Added console output on start

--- src/main/java/net/inceptioncloud/installer/Main.java
+++ src/main/java/net/inceptioncloud/installer/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
     public static void main (String[] args)
     {
+        System.out.println("Running Dragonfly Installation Wizard");
         MinecraftModInstaller.INSTANCE.init();
     }
 }
