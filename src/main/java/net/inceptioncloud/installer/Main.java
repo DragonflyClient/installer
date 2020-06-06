db84d398af0c7d913e0ad1c8dd1406bff89b63fe
theincxption <theincxption@gmail.com>
2020-06-06 12:47:10 +0200

    Another Test

--- src/main/java/net/inceptioncloud/installer/Main.java
+++ src/main/java/net/inceptioncloud/installer/Main.java
@@ -4,8 +4,6 @@ public class Main
 {
     public static void main (String[] args)
     {
-        System.out.println("Running Dragonfly Installation Wizard");
-        System.out.println("Baum");
         MinecraftModInstaller.INSTANCE.init();
     }
 }
