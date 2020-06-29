5fd21f557b3a855669ae3da6e7e3b8ff9fb76679
verity <joshua-schmidt@online.de>
2020-06-29 11:11:07 +0200

    Added copyBack function to CacheManager

--- src/main/java/net/inceptioncloud/installer/CacheManager.kt
+++ src/main/java/net/inceptioncloud/installer/CacheManager.kt
@@ -3,11 +3,11 @@ package net.inceptioncloud.installer
 import java.io.File
 
 object CacheManager {
-    private val tmpDir = File("${System.getProperty("java.io.tmpdir")}/DragonflyInstaller")
+    private val tmpDir = File("${System.getProperty("java.io.tmpdir")}\\DragonflyInstaller")
 
     fun copyFile(file: File): Boolean {
         return if (checkFolder()) {
-            file.copyTo(File("$tmpDir/${file.name}"), true)
+            file.copyTo(File("$tmpDir\\${file.name}"), true)
             true
         } else {
             false
@@ -16,22 +16,34 @@ object CacheManager {
 
     fun copyFolder(folder: File, name: String): Boolean {
         return if (checkFolder()) {
-            File("$tmpDir/$name/").mkdirs()
+            File("$tmpDir\\$name\\").mkdirs()
             for (file in folder.listFiles()) {
                 if (file.isDirectory) {
-                    File("$tmpDir/$name/${file.name}")
                     for (file2 in file.listFiles()) {
-                        file2.copyTo(File("$tmpDir/$name/${file.name}/${file2.name}"), true)
+                        file2.copyTo(File("$tmpDir\\$name\\${file.name}\\${file2.name}"), true)
                     }
                 } else {
-                    file.copyTo(File("$tmpDir/$name/${file.name}"), true)
+                    file.copyTo(File("$tmpDir\\$name\\${file.name}"), true)
                 }
             }
             true
         } else {
             false
         }
+    }
 
+    fun copyBack(target: String, folder: File) {
+        val targetFolder = File("$tmpDir/$target/")
+
+        for (file in targetFolder.listFiles()) {
+            if (file.isDirectory) {
+                for (file2 in file.listFiles()) {
+                    file2.copyTo(File("${folder.absolutePath}\\${file.name}\\${file2.name}"), true)
+                }
+            } else {
+                file.copyTo(File("${folder.absolutePath}\\${file.name}"), true)
+            }
+        }
     }
 
     fun deleteFolder() {
